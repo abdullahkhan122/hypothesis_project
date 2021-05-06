43af4d2e296c21693c71ab5276200f83bed1ad7a
abdullahkhan122 <83785828+abdullahkhan122@users.noreply.github.com>
2021-05-06 15:34:30 -0400

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,235 @@
+print("-----------------------------------------------------")
+print("Snow Months: January | February | March ")
+print("")
+print("Non-Snow Months: April | May | June | July ")
+print(" August | September | October | November | December")
+print("-----------------------------------------------------")
+
+
+#Assigning each month to their sales that were recorded
+January = 103060.25
+February = 100792.96
+March = 104195.52
+April = 152396.52
+May = 75528.40
+June = 97534.75
+July = 104597.10
+August = 103201.50
+September = 76370.20
+October = 60290.50
+November = 81216.45
+December = 95825.60
+
+#Asking the user for the months
+snowKey = str(input("Enter a Snow Month: "))
+nonsnowKey = str(input("Enter a Non-Snow Month: "))
+
+
+#Comparing the sales of the months with the sales of January
+if snowKey == "January" and nonsnowKey == "April":
+	if January > 2*April:
+		print("The sales of January was double the sales of April")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of April")
+		print("The hypothesis is False")
+
+elif snowKey == "January" and nonsnowKey == "May":
+	if January > 2*May:
+		print("The sales of January was double the sales of May")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of May")
+		print("The hypothesis is False")
+elif snowKey == "January" and nonsnowKey == "June":
+	if January > 2*June:
+		print("The sales of January was double the sales of June")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of June")
+		print("The hypothesis is False")
+elif snowKey == "January" and nonsnowKey == "July":
+	if January > 2*July:
+		print("The sales of January was double the sales of July")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of July")
+		print("The hypothesis is False")
+elif snowKey == "January" and nonsnowKey == "August":
+	if January > 2*August:
+		print("The sales of January was double the sales of August")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of August")
+		print("The hypothesis is False")
+elif snowKey == "January" and nonsnowKey == "September":
+	if January > 2*September:
+		print("The sales of January was double the sales of September")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of September")
+		print("The hypothesis is False")
+elif snowKey == "January" and nonsnowKey == "October":
+	if January > 2*October:
+		print("The sales of January was double the sales of October")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of October")
+		print("The hypothesis is False")
+elif snowKey == "January" and nonsnowKey == "November":
+	if January > 2*November:
+		print("The sales of January was double the sales of November")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of November")
+		print("The hypothesis is False")
+elif snowKey == "January" and nonsnowKey == "December":
+	if January > 2*December:
+		print("The sales of January was double the sales of December")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of January was not double the sales of December")
+		print("The hypothesis is False")
+
+#Comparing the sales of the months with the sales of February
+elif snowKey == "February" and nonsnowKey == "April":
+	if February > 2*April:
+		print("The sales of February was double the sales of April")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of April")
+		print("The hypothesis is False")
+
+elif snowKey == "February" and nonsnowKey == "May":
+	if February > 2*May:
+		print("The sales of February was double the sales of May")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of May")
+		print("The hypothesis is False")
+elif snowKey == "February" and nonsnowKey == "June":
+	if February > 2*June:
+		print("The sales of February was double the sales of June")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of June")
+		print("The hypothesis is False")
+elif snowKey == "February" and nonsnowKey == "July":
+	if February > 2*July:
+		print("The sales of February was double the sales of July")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of July")
+		print("The hypothesis is False")
+elif snowKey == "February" and nonsnowKey == "August":
+	if February > 2*August:
+		print("The sales of February was double the sales of August")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of August")
+		print("The hypothesis is False")
+elif snowKey == "February" and nonsnowKey == "September":
+	if February > 2*September:
+		print("The sales of February was double the sales of September")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of September")
+		print("The hypothesis is False")
+elif snowKey == "February" and nonsnowKey == "October":
+	if February > 2*October:
+		print("The sales of February was double the sales of October")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of October")
+		print("The hypothesis is False")
+elif snowKey == "February" and nonsnowKey == "November":
+	if February > 2*November:
+		print("The sales of February was double the sales of November")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of November")
+		print("The hypothesis is False")
+elif snowKey == "February" and nonsnowKey == "December":
+	if February > 2*December:
+		print("The sales of February was double the sales of December")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of February was not double the sales of December")
+		print("The hypothesis is False")
+
+#Comparing the sales of the months with the sales of March
+elif snowKey == "March" and nonsnowKey == "April":
+	if March > 2*April:
+		print("The sales of March was double the sales of April")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of April")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "May":
+	if March > 2*May:
+		print("The sales of March was double the sales of May")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of May")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "June":
+	if March > 2*June:
+		print("The sales of March was double the sales of June")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of June")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "July":
+	if March > 2*July:
+		print("The sales of March was double the sales of July")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of July")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "August":
+	if March > 2*August:
+		print("The sales of March was double the sales of August")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of August")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "September":
+	if March > 2*September:
+		print("The sales of March was double the sales of September")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of September")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "October":
+	if March > 2*October:
+		print("The sales of March was double the sales of October")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of October")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "November":
+	if March > 2*November:
+		print("The sales of March was double the sales of November")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of November")
+		print("The hypothesis is False")
+
+elif snowKey == "March" and nonsnowKey == "December":
+	if March > 2*December:
+		print("The sales of March was double the sales of December")
+		print("The hypothesis is True.")
+	else:
+		print("The sales of March was not double the sales of December")
+		print("The hypothesis is False")
+
+else:
+	print("The month entered is invalid. Please try again. ") #Asking the user to enter a valid month
+
